46e3583ae0cb79796e673aac893b8fc1f8df739e
Bizki1337 <nightingale1749@yandex.ru>
2021-02-05 16:55:53 +0300

    Initial Commit

---
+++ src/components/UI/Backdrop/Backdrop.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import classes from './Backdrop.module.css'
+
+const Backdrop = (props) => <div className={classes.Backdrop} onClick={props.onClose} />
+
+export default Backdrop
